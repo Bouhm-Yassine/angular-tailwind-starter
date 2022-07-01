39208effdb85da84310c6e3cf4d68040c2765689
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-07-01 12:52:19 +0100

    add theme preview components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { PreviewContainersComponent } from './preview-containers/preview-containers.component';
+import { PreviewControlsComponent } from './preview-controls/preview-controls.component';
+import { PreviewGeneralComponent } from './preview-general/preview-general.component';
+import { PreviewTypographyComponent } from './preview-typography/preview-typography.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreviewContainersComponent,
+    PreviewControlsComponent,
+    PreviewGeneralComponent,
+    PreviewTypographyComponent
   ],
   imports: [
     SharedModule,
