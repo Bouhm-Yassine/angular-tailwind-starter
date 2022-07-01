404355f72be4ea1d06ea528b4f2f2db959fcfe4b
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-07-01 13:13:22 +0100

    add dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, HostListener } from '@angular/core';
+import { Component, ViewChild, HostListener, HostBinding, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatSidenav } from '@angular/material/sidenav';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 @Component({
@@ -6,10 +7,19 @@ import { MatTabChangeEvent } from '@angular/material/tabs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @HostBinding('class') className = '';
+
   activeTab: string;
+  toggleControl = new FormControl(false);
 
   handleTabChange(tab: MatTabChangeEvent) {
     this.activeTab = tab.tab.ariaLabel;
+  }
+  ngOnInit(): void {
+    this.toggleControl.valueChanges.subscribe((darkMode) => {
+      const darkClassName = 'dark-mode';
+      this.className = darkMode ? darkClassName : '';
+    });
   }
 }
